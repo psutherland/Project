27fedea82254b6d848e902ed951a8a05d1242f2b
Preston Sutherland <prestonsutherland1@gmail.com>
2013-11-28 10:24:24 -0600

    added comics and partials

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,5 +1,5 @@
 class Comic < ActiveRecord::Base
-  attr_accessible :description, :name, :price, :publisher, :stock_quanitity
+  attr_accessible :description, :name, :price, :publisher, :stock_quanitity, :url, :condition
 
   has_many :comic_orders
 end
